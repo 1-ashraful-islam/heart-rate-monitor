9948f622e355339bca3156e6c95140ca1c332588
Ashraful Islam <ashraful.meche@gmail.com>
2024-05-29 22:40:38 +0000

    implements video capture, writing, annotation

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
+# Heart rate monitor
+
+This project parses video to monitor heart rate in BPM.
+
 ## Prerequisits
 
-Build OpenCV [linux](https://docs.opencv.org/4.x/d7/d9f/tutorial_linux_install.html). There is a convenience script that will fetch and build and install the opencv in the local directory using the following commands:
+Build OpenCV [(linux reference)](https://docs.opencv.org/4.x/d7/d9f/tutorial_linux_install.html). There is a convenience script that will fetch and build and install the opencv in the local directory using the following commands:
 
 ```sh
 cd third_party/opencv
 ./build-install-opencv.sh
 ```
+
+## Resources
+
+Install bazelisk: [bazelisk reference](https://docs.bazel.build/versions/5.4.1/install-bazelisk.html)
--- main/main.cc
+++ main/main.cc
@@ -1,13 +1,72 @@
-#include <iostream>
 #include <opencv2/opencv.hpp>
+#include <vector>
+#include <fmt/core.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-  cv::Mat image = cv::Mat::zeros(100, 100, CV_8UC3);
-  if (image.empty()) {
-      std::cerr << "Could not create image" << std::endl;
-      return -1;
+  // read the video file from the argument
+  if (argc != 2)
+  {
+    fmt::print(stderr, "Required argument: <video_file>");
+    return -1;
   }
-  std::cout << "Image created successfully" << std::endl;
+
+  auto source = cv::VideoCapture(argv[1]);
+
+  if (!source.isOpened())
+  {
+    fmt::print(stderr, "Error: VideoCapture failed to open source: {}", argv[1]);
+    return -1;
+  }
+
+  // video writer paramters
+  auto videoOutFile = "/workspaces/CPP/src/workspace/heart-rate-monitor/processed_video.mp4";
+  auto fps = 30;
+  auto frameWidth = source.get(cv::CAP_PROP_FRAME_WIDTH);
+  auto frameHeight = source.get(cv::CAP_PROP_FRAME_HEIGHT);
+  cv::Size frameSize(frameWidth, frameHeight);
+  cv::VideoWriter videoOut(videoOutFile, cv::VideoWriter::fourcc('X', 'V', 'I', 'D'), fps, frameSize);
+
+  // ROI
+  cv::Point top_left(345, 185);
+  cv::Point bottom_right(580, 400);
+
+  // Create a mask with ROI
+  cv::Mat mask = cv::Mat::zeros(frameSize, CV_8UC1);
+  cv::rectangle(mask, top_left, bottom_right, cv::Scalar(255), cv::FILLED);
+
+  // annotation text parameters
+  int fontFace = cv::FONT_HERSHEY_PLAIN;
+  double fontScale = 1.5;
+  cv::Scalar fontColor(0, 255, 0);
+  int thickness = 2;
+  cv::Point textOrigin(50, 100);
+
+  // performance stat
+  cv::TickMeter tickMeter;
+
+  std::vector<double> green_t;
+  cv::Mat frame;
+  int count = 0;
+
+  while (true)
+  {
+    tickMeter.start();
+    count++;
+    source >> frame;
+    if (frame.empty())
+    {
+      fmt::print("No more frames available. Processed frame count: {}", count - 1);
+      break;
+    }
+    auto average = cv::mean(frame, mask);
+
+    auto text = fmt::format("Red: {:.2f} Green: {:.2f} Blue: {:.2f} FPS: {:.2f}", average[2], average[1], average[0], tickMeter.getFPS());
+    cv::rectangle(frame, top_left, bottom_right, fontColor, thickness, cv::LINE_8);
+    cv::putText(frame, text, textOrigin, fontFace, fontScale, fontColor, thickness, cv::LINE_AA);
+    videoOut.write(frame);
+    tickMeter.stop();
+  }
+
   return 0;
 }
